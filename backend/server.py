914049fef1aa82b76fb2bc613f2298079adb7fc1
hammadaslam1 <hammadaslam308@gmail.com>
2023-12-05 23:04:42 +0500

    edited _document.jsx

--- backend/server.py
+++ backend/server.py
@@ -1,13 +1,15 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 # app instance
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/api/home", methods=['GET'])
 def return_home():
     return jsonify({
-        'message': 'hello'
+        'message': 'Reviews Classifier'
     })
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug = True, port=8080)
