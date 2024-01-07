3899fc4c94fa78a597abba7cf947669bca6048cf
hammadaslam1 <hammadaslam308@gmail.com>
2024-01-07 11:08:59 +0500

    added react app as frontend

--- backend/server.py
+++ backend/server.py
@@ -15,6 +15,17 @@ def return_home():
     # return jsonify(data)
     return data
 
+@app.route("/sentiments", methods=['GET'])
+def reviews_sentiment():
+    with open('./backend/datasets/combined_data.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    reviews = [item['reviews'] for item in data]
+    print(reviews)
+    b=[]
+    for a in reviews:
+        b.append(a)
+    # return jsonify(data)
+    return b[0]
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
