93a0241db6820782fb3f1c4c43c5bfe0990c76f8
hammadaslam1 <hammadaslam308@gmail.com>
2024-03-10 22:28:36 +0500

    applied BERTopic and removed duplicate data from dataset

--- backend/datasets/remove_data.py
+++ backend/datasets/remove_data.py
@@ -1,14 +1,28 @@
 import json
 
 
-def filter_records(data):
+def filter_records(edata):
     filtered_data = []
     # print(data)
-    for product_data in data:
+    for product_data in edata:
         # print(product_data["reviews"])
         has_review_body = False
         rev = []
         # print(product_data["reviews"])
+        product_data['product_title'] = list(
+            set(product_data['product_title']))
+        product_data['all_products_href'] = list(
+            set(product_data['all_products_href']))
+        product_data['product_rating_points'] = list(
+            set(product_data['product_rating_points']))
+        product_data['product_ratings'] = list(
+            set(product_data['product_ratings']))
+        product_data['product_price'] = list(
+            set(product_data['product_price']))
+        product_data['product_images_src'] = list(
+            set(product_data['product_images_src']))
+        product_data['product_description'] = list(
+            set(product_data['product_description']))
         for review in product_data["reviews"]:
             # print(review)
             # print(type(review))# Handle potential missing "reviews" key
@@ -25,17 +39,17 @@ def filter_records(data):
 
 # ... (Rest of the code remains the same)
 # Load the JSON data
-with open('./backend/datasets/categories/allFiles/mens_wrist_watches.json', 'r', encoding='utf-8') as f:
+with open('./backend/datasets/categories/womens_fashion/women_casual_jackets.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
 
 # Combine records (if applicable)
 # ...
 
 # Filter records without review bodies
-filtered_data = filter_records(data)
+filtered = filter_records(data)
 
-print(len(filtered_data))
+print(len(filtered))
 
 # Write the filtered data to a new JSON file
-with open('./backend/datasets/categories/allFiles/mens_wrist_watches.json', 'w', encoding='utf-8') as f:
-    json.dump(filtered_data, f, indent=4)
+with open('./backend/datasets/categories/womens_fashion/women_casual_jackets.json', 'w', encoding='utf-8') as f:
+    json.dump(filtered, f, indent=4)
