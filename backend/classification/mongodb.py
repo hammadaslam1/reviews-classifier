6b3efd498675db43e58e104687423dec57268791
Muhammad Hammad Aslam <hammadaslam308@gmail.com>
2024-03-26 14:06:29 +0500

    embedded gemini with lemmatizing function

--- backend/classification/mongodb.py
+++ backend/classification/mongodb.py
@@ -1,9 +1,9 @@
 import os
-import pymongo
+# import pymongo
 import json
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-db = myclient['OpinioMine']
+# myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+# db = myclient['OpinioMine']
 
 
 json_dir = './backend/datasets/categories/allFiles'
@@ -11,32 +11,14 @@ json_dir = './backend/datasets/categories/allFiles'
 
 for filename in os.listdir(json_dir):
     if filename.endswith('.json'):
-        collection_name = os.path.splitext(filename)[0]  # Use filename as collection name
-        collection = db[collection_name]
-        with open(os.path.join(json_dir, filename), 'r') as file:
-            data = json.load(file)
-            collection.insert_many(data)
+        with open(json_dir+'/'+filename, 'r') as f:
+            data = json.load(f)
+            print(filename)
+            print(len(data))
+        # collection_name = os.path.splitext(filename)[0]  # Use filename as collection name
+        # collection = db[collection_name]
+        # with open(os.path.join(json_dir, filename), 'r') as file:
+        #     data = json.load(file)
+        #     collection.insert_many(data)
 
 print('sent successfully')
-# print(db)
-
-# with open('./backend/datasets/aaaa.json', 'r', encoding='utf-8') as f:
-#     data = json.load(f)
-
-# mydb = myclient["hammad"]
-# mycol = mydb["customers"]
-
-# mydict = { "name": "John", "address": "Highway 37" }
-
-# # with open('./backend/datasets/categories/electronics/camera_and_photo.json', 'w') as f:
-# #     json.dump(mydict, f, indent=4)
-# for item in data:
-#     mycol.insert_one(item)
-#     print(item)
-
-# print(myclient.list_database_names())
-
-
-# dblist = myclient.list_database_names()
-# if "local" in dblist:
-#     print("The database exists.")
--- backend/classification/sentiments.py
+++ backend/classification/sentiments.py
@@ -3,37 +3,44 @@ import json
 import glob
 import re
 from textblob import TextBlob
+import sys
+
+sys.path.append("./backend/practice")
+import geminiModel
 
 # gensim
-import gensim
-import gensim.corpora as corpora
-from gensim.utils import simple_preprocess
-from gensim.models import CoherenceModel
+# import gensim
+# import gensim.corpora as corpora
+# from gensim.utils import simple_preprocess
+# from gensim.models import CoherenceModel
 
 # spacy
 import spacy
+import spacy.cli
+import nltk
+# spacy.cli.download("en_core_web_sm")
 from nltk.corpus import stopwords
+nltk.download('vader_lexicon')
 
 # sentiments
 from nltk.sentiment import SentimentAnalyzer, SentimentIntensityAnalyzer
 
 # vis
-import pyLDAvis
-import pyLDAvis.gensim
+# import pyLDAvis
+# import pyLDAvis.gensim
 
 # import warnings
 # warnings.filterwarnings('ignore', category=DeprecationWarning)
 
 
 def load_data(file):
-    with open(file, 'r', encoding='utf-8') as f:
+    with open(file, "r", encoding="utf-8") as f:
         data = json.load(f)
     return data
 
 
 # data_array = []
-data = load_data(
-    './backend/datasets/categories/electronics/camera_and_photo.json')
+data = load_data("./backend/datasets/categories/electronics/camera_and_photo.json")
 # print(data)
 
 # data_array.append(data)
@@ -41,46 +48,47 @@ data = load_data(
 # print(data_array)
 
 
-def gen_words(texts):
-    final = []
-    new = gensim.utils.simple_preprocess(texts, deacc=True)
-    # print(new)
-    blob = TextBlob(' '.join(new))
-    final = blob.noun_phrases
-    return final
+# def gen_words(texts):
+#     final = []
+#     new = gensim.utils.simple_preprocess(texts, deacc=True)
+#     # print(new)
+#     blob = TextBlob(" ".join(new))
+#     final = blob.noun_phrases
+#     return final
 
 
-def lemmatization(texts, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
-    nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
+def lemmatization(texts, allowed_postags=["NOUN", "ADJ", "VERB", "ADV"]):
+    nlp = spacy.load("en_core_web_sm", disable=["parser", "ner"])
     text_out = []
 
     for text in texts["reviews"]:
-        new_record = re.sub(' +', ' ', text["review_body"])
+        new_record = re.sub(" +", " ", text["review_body"])
         doc = nlp(new_record)
-        sentiment = ''
+        sentiment = ""
         sid = SentimentIntensityAnalyzer()
         scores = sid.polarity_scores(new_record)
         if scores["compound"] > 0.5:
-            sentiment = 'Strictly Positive'
+            sentiment = "Strictly Positive"
         elif scores["compound"] > 0:
-            sentiment = 'Neutrally Positive'
+            sentiment = "Neutrally Positive"
         elif scores["compound"] > -0.5:
-            sentiment = 'Neutrally Negative'
+            sentiment = "Neutrally Negative"
         elif scores["compound"] >= -1:
-            sentiment = 'Strictly Negative'
+            sentiment = "Strictly Negative"
 
         text["sentiment"] = sentiment
         for token in doc:
             filtered_list = [token for token in doc if not token.is_stop]
-            lemmas = [
-                f"{token.lemma_}"
-                for token in filtered_list
-            ]
-            string = ' '.join(lemmas)
+            lemmas = [f"{token.lemma_}" for token in filtered_list]
+            string = " ".join(lemmas)
         final = " ".join(lemmas)
-        stem = gen_words(final)
+        print("################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################")
+        # print(final)
+        stem = geminiModel.gemini(text["review_body"])
         text["review_topics"] = stem
-    text_out.append(final)
+        print(text['review_topics'])
+    # text_out.append(final)
+    # print
     return text_out
 
 
@@ -90,5 +98,5 @@ for item in data:
     lemma_array.append(lemmatizedText)
 
 
-with open('./backend/datasets/categories/electronics/camera_and_photo.json', 'w') as f:
-    json.dump(data, f, indent=4)
+# with open("./backend/datasets/categories/electronics/camera_and_photo.json", "w") as f:
+#     json.dump(data, f, indent=4)
